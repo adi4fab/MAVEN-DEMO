285952141a8eed86aa23d090382a601086e59d56
ADI <fabregas4aditya@gmail.com>
2021-07-11 12:05:05 +0530

    all

---
+++ src/main/java/com/devops/first_maven_aditya/App.java
@@ -0,0 +1,13 @@
+package com.devops.first_maven_aditya;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
